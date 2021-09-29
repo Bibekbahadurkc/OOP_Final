f2d8751d79717b3473fc6dda931085573e088afa
Bibek <Bibekbahadurkc@gmail.com>
2021-09-29 17:05:34 +0545

    testing

---
+++ MCQ/src/testing/AdminTestCase.java
@@ -0,0 +1,17 @@
+package testing;
+
+//import org.junit.jupiter.api.Test;
+
+
+
+/*
+ * class AdminTestCase {
+ * 
+ * @Test void test() { String id = "samir"; String pw = "sam123"; LoginMain l =
+ * new LoginMain(); boolean result = l.checklogin(id,pw);
+ * assertEquals(true,result); }
+ * 
+ * 
+ * 
+ * }
+ */
